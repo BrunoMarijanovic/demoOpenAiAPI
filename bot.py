e1ef9ce0d91b386a38254e1647ee6d5e09df6492
BrunoMarijanovic <brunoandresmarijanovic@gmail.com>
2023-02-21 18:48:08 +0100

    'first'

---
+++ bot.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import openai
+
+openai.api_key = "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"
+
+ventana = tk.Tk()
+ventana.geometry("675x350")
+
+def responder():
+    pregunta = texto.get("1.0", tk.END)
+
+    completion = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=pregunta)
+    respuesta.config(text=completion.choices[0].text)
+
+titulo = tk.Label(ventana, text="Escribe tu pregunta")
+titulo.grid(row=0, column=0, padx=10, pady=10)
+
+texto = tk.Text(ventana, height=5, border=5, wrap="word")
+texto.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+
+boton = tk.Button(ventana, text="Enviar", command=responder)
+boton.grid(row=2, column=0, padx=10, pady=10)
+
+respuesta = tk.Label(ventana, text="")
+respuesta.grid(row=3, column=0, padx=10, pady=10)
+
+ventana.mainloop()
